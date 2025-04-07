3bd9feb6eed3022885693f28c9bda12350245084
motorniko <albertolagos59@gmail.com>
2025-04-07 00:11:05 -0400

    generando cambios

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenidos a Screen Match");
-        System.out.println("Pelicula : Matrix");
+        System.out.println("Película: Matrix");
 
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElplan= true;
-        String sinopsis = "\n"+"\n" + """
-                          Accion aventura de ciencia ficcion, donde un programador descubre que el mundo es una simulacion creada por maquinas.
-                          El programa Matrix es un sistema de control mental que mantiene a los humanos en un estado de ignorancia.
-                          Neo, el protagonista, se une a un grupo de rebeldes para luchar contra las maquinas y descubrir la verdad sobre su realidad.
-                          
-                          """ + "Fecha de lanzamiento: " + fechaDeLanzamiento + "\n";
+        boolean incluidoEnElPlan = true;
 
-        double calificacion = 8.7 ;
-        double media = (8.2 + 6.8 + 9.0) / 3;
-        int clasificacion = (int) (media / 2);
+        String sinopsis = "\n\n" +
+                "Acción, aventura de ciencia ficción, donde un programador descubre que el mundo es una simulación creada por máquinas.\n" +
+                "El programa Matrix es un sistema de control mental que mantiene a los humanos en un estado de ignorancia.\n" +
+                "Neo, el protagonista, se une a un grupo de rebeldes para luchar contra las máquinas y descubrir la verdad sobre su realidad.\n" +
+                "Fecha de lanzamiento: " + fechaDeLanzamiento + "\n";
 
+        double calificacion = 8.7;
+        double promedio = (8.2 + 6.8 + 9.0) / 3;
+        int clasificacion = (int) (promedio / 2);
 
-        System.out.println("calificacion: " + calificacion);
-        System.out.println("Clasificacion: " + clasificacion);
+        System.out.println("Calificación: " + calificacion);
+        System.out.println("Clasificación: " + clasificacion);
         System.out.println("Sinopsis: " + sinopsis);
 
-
-        if (incluidoEnElplan == true) {
+        if (incluidoEnElPlan) {
             System.out.println("Incluido en el plan");
-        }
-        else {
+        } else {
             System.out.println("No incluido en el plan");
-
         }
     }
 }
